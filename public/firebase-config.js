dd0aeba28ed6e628c3b26d33ab0d213f4922feca
kg07nurislam <152377115+kg07nurislam@users.noreply.github.com>
2025-03-31 18:24:17 +0600

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,19 +1,15 @@
-// Import Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
 
-// Firebase'дин конфигурациясы
 const firebaseConfig = {
-    apiKey: "AIzaSyCGeTpv7...",
-    authDomain: "vgapp-db.firebaseapp.com",
-    projectId: "vgapp-db",
-    storageBucket: "vgapp-db.appspot.com",
-    messagingSenderId: "246581465852",
-    appId: "1:246581465852:web:a29e323c2409e5e1e73d79"
+  apiKey: "AIzaSyCGeTpv7...",
+  authDomain: "vgapp-db.firebaseapp.com",
+  projectId: "vgapp-db",
+  storageBucket: "vgapp-db.appspot.com",
+  messagingSenderId: "246581465852",
+  appId: "1:246581465852:web:a29e323c2409e5e1e73d79",
+  measurementId: "G-NP3MFTQ8KN"
 };
 
-// Firebase'ди баштоо
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+export const db = getFirestore(app);
