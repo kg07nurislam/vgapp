17021565273cde70e8a79bbbe97d23558cee0c55
kg07nurislam <152377115+kg07nurislam@users.noreply.github.com>
2025-03-31 08:11:24 +0600

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
+
+// Firebase'дин конфигурациясы
+const firebaseConfig = {
+  apiKey: "AIzaSyCGeTpv7iHCvyobErcEiVd8by0Cwb_d_Lc",
+  authDomain: "vgapp-db.firebaseapp.com",
+  projectId: "vgapp-db",
+  storageBucket: "vgapp-db.appspot.com",
+  messagingSenderId: "246581465852",
+  appId: "1:246581465852:web:a29e323c2409e5e1e73d79",
+};
+
+// Firebase'ди баштоо
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
