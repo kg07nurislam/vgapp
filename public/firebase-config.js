8d1bba8a8bb52352161bf7af292d7cae4437c9f0
kg07nurislam <152377115+kg07nurislam@users.noreply.github.com>
2025-03-31 18:11:12 +0600

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,14 +1,15 @@
+// Import Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
 
 // Firebase'дин конфигурациясы
 const firebaseConfig = {
-  apiKey: "AIzaSyCGeTpv7iHCvyobErcEiVd8by0Cwb_d_Lc",
-  authDomain: "vgapp-db.firebaseapp.com",
-  projectId: "vgapp-db",
-  storageBucket: "vgapp-db.appspot.com",
-  messagingSenderId: "246581465852",
-  appId: "1:246581465852:web:a29e323c2409e5e1e73d79",
+    apiKey: "AIzaSyCGeTpv7...",
+    authDomain: "vgapp-db.firebaseapp.com",
+    projectId: "vgapp-db",
+    storageBucket: "vgapp-db.appspot.com",
+    messagingSenderId: "246581465852",
+    appId: "1:246581465852:web:a29e323c2409e5e1e73d79"
 };
 
 // Firebase'ди баштоо
